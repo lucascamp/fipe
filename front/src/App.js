a82033824211c69f75ab57091053cd98f1326a2b
Lucas Campos <lucas.campos@sympla.com.br>
2020-11-19 23:52:07 -0300

    dropdown marcas

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Button, Card, ListGroup, ListGroupItem, Dropdown } from 'react-bootstrap';
 
 class App extends Component {
 
     constructor(props) {
       super(props);
       this.state = {
-        items: [],
+        cars: [],
         isLoaded: false,
       }
     }
@@ -20,50 +20,73 @@ class App extends Component {
             .then(res => res.json())
             .then(json =>
                 this.setState ({  
-                  items: json,
-                  isLoaded: true,
+                  cars: json,
+                  isLoadedCars: true,
+                }) )
+                
+        fetch('http://fipeapi.appspot.com/api/1/carros/marcas.json')
+            .then(res => res.json())
+            .then(json =>
+                this.setState ({  
+                  brands: json,
+                  isLoadedBrands: true,
                 }) )
     }
 
     render() {
 
-        var { isLoaded, items } = this.state;
+        var { isLoadedCars, cars, isLoadedBrands, brands } = this.state;
 
-        if(!isLoaded) {
+        if(!isLoadedCars) {
           return <div> loading ...</div>
         }
 
         else {
-          console.log(items);
+          console.log(brands);
           return (
             <div className="App">
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="https://3.bp.blogspot.com/-2-HOM_lKNOM/VYhYyqKZe3I/AAAAAAACJwQ/gyFXu4i6y2o/s1600/Fiat-Palio-Fire-2016%2B%25282%2529.jpg" />
+
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                Selecione a marca
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                          { brands.map( brand => (
+                                <Dropdown.Item href="#">{ brand.name }</Dropdown.Item>
+                              )
+                            )
+                          };
+            </Dropdown.Menu>
+          </Dropdown>
+                
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://3.bp.blogspot.com/-2-HOM_lKNOM/VYhYyqKZe3I/AAAAAAACJwQ/gyFXu4i6y2o/s1600/Fiat-Palio-Fire-2016%2B%25282%2529.jpg" />
                   
-                  <Card.Body>
-                    <Card.Title>{ items.marca }</Card.Title>
-                    <Card.Text>
-                      { items.name }
-                    </Card.Text>
-                  </Card.Body>
+            <Card.Body>
+              <Card.Title>{ cars.marca }</Card.Title>
+              <Card.Text>
+                { cars.name }
+              </Card.Text>
+            </Card.Body>
 
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>Código FIPE: { items.fipe_codigo }</ListGroupItem>
-                    <ListGroupItem>Referência:  { items.referencia }</ListGroupItem>
-                    <ListGroupItem>Combustível: { items.fipe_codigo }</ListGroupItem>
-                    <ListGroupItem>Ano:         { items.ano_modelo }</ListGroupItem>
-                    <ListGroupItem>Preço:       { items.preco }</ListGroupItem>
-                  </ListGroup>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Código FIPE: { cars.fipe_codigo }</ListGroupItem>
+              <ListGroupItem>Referência:  { cars.referencia }</ListGroupItem>
+              <ListGroupItem>Combustível: { cars.fipe_codigo }</ListGroupItem>
+              <ListGroupItem>Ano:         { cars.ano_modelo }</ListGroupItem>
+              <ListGroupItem>Preço:       { cars.preco }</ListGroupItem>
+            </ListGroup>
 
-                  <Card.Body>
-                    <Button variant="info">Info</Button>{' '}
-                    <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-                  </Card.Body>
+            <Card.Body>
+              <Button variant="info">Info</Button>{' '}
+              <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+            </Card.Body>
 
-                  <Card.Body>
-                    <Card.Link href="#">Link</Card.Link>
-                  </Card.Body>
-                </Card>
+            <Card.Body>
+              <Card.Link href="#">Link</Card.Link>
+            </Card.Body>
+          </Card>
             </div>
           );
         }
